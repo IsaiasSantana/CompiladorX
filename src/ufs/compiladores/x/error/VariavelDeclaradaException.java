1f024af43d08ff54fb26dff25d9aec909d16589d
Isaías Santana <Isaas Santana>
2016-09-16 00:33:00 -0300

    remocao print

--- src/ufs/compiladores/x/error/VariavelDeclaradaException.java
+++ src/ufs/compiladores/x/error/VariavelDeclaradaException.java
@@ -1,5 +1,11 @@
 package ufs.compiladores.x.error;
 
+/**
+ * Lança uma exceção quando se declara uma outra variável com o mesmo nome.
+ * @author Isaías
+ * @since 16/09/2016 00:32
+ * @version 0.1
+ */
 public class VariavelDeclaradaException extends Exception
 {
 	public VariavelDeclaradaException(String mensagem) {
